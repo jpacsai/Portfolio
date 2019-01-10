73279904fc8a2cc0ae545f1a042327eeff75c3b6
unknown <judit.pacsai@gmail.com>
2019-01-10 15:37:17 +0100

    feat: add lorem ipsum text to about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="view about">
     <img :src="selfie" alt="photo of Judit" />
+    <section>
+      <p>Lorem ipsum dolor sit amet, vim ut adhuc postulant. Nec integre laoreet in, ut ubique ridens pro. Ad mel iudico insolens mediocrem. Ut vim vocent ancillae rationibus, no eos illud impedit.</p>
+      <p>Denique minimum mel at. Adhuc porro deterruisset id pri, id discere sanctus civibus qui, id sit choro dolor habemus. Justo accusam pro eu. Viris aperiri ex per, et mundi prompta volumus per, eum ut lobortis dissentiunt. Vim iisque mentitum deseruisse et, id utamur inciderint sed. Dolor quaeque feugiat nec at, hinc rebum percipitur an his.</p>
+    </section>
   </div>
 </template>
 
