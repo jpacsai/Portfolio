f066224c1f57c71551db4ca552b337369af6ff09
unknown <judit.pacsai@gmail.com>
2019-01-10 16:28:35 +0100

    style: set about height

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -2,7 +2,7 @@
   <svg id="svg">
     <defs></defs>
     <g class="desk">
-    
+      <rect x="41.577" y="36.952" width="73.327" height="67.28" fill="#f3f" stroke-width="0"/>
     </g>
   </svg>
 </template>
